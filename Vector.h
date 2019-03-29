2f27d6f96024db98cfa0dbf86cb29f46ade35eed
John <jonnyuz99@gmail.com>
2019-03-29 21:37:26 +0300

    Short library

--- Vector.h
+++ Vector.h
@@ -1,15 +1,3 @@
-/*
--------------------QUESTIONS--------------
-
-1) 	Если в классе есть указатель на файл, как лучше 
-	его инициализировать в конструкторе?
-
-2)	Можно ли как-нибудь узнать номер строки
-	вызова без передачи аргумента __ЛАЙН__ 
-	в конструкторе?
-
-*/
-
 #ifndef VECTOR_H_
 #define VECTOR_H_
 
@@ -28,28 +16,29 @@ class MyVector
 		int 			size;
 		MyType *		data;
 		std::fstream 	file;
-		std::string str;
 
 	public:
 		MyVector		();
 		MyVector		(int init_size);
 		MyVector        (const MyVector& vector);
+		MyVector		(MyVector && vector);
 		~MyVector		();
 
 		friend std::istream &	operator>>	(std::istream & stream, MyVector & V);
 		friend std::ostream &	operator<<	(std::ostream & stream, MyVector & V);
 		friend MyVector 		operator*	(MyType number, const MyVector & rhs);
+
 		MyVector &				operator=	(const MyVector & rhs);
+		MyVector &				operator=	(MyVector && rhs);
+		const MyType &			operator[]	(int index) const;
+		MyType &				operator[]	(int index);
 		MyVector 				operator+	(const MyVector & rhs);
 		MyVector 				operator-	(const MyVector & rhs);
 		MyVector 				operator*	(MyType number);
 		MyVector 				operator/	(MyType number);
-		MyType &				operator[]	(int index);
-		void *					operator new(size_t, void * ptr);
-
+		
 		void					Show		(const MyVector & V);	
 		void 					VSwap		(MyVector & V);
-		MyType &				Element		(int index);
 };
 
 #endif
